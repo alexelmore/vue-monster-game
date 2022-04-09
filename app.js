35541c414452e63ed826ca2c2e850337c8586a7c
aelmore49 <alexelmore49@gmail.com>
2022-04-09 09:44:19 -0500

    Setup for attacking player

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
+// Function for generating a random value 
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
 Vue.createApp({
   data() {
     return {
-        playerHealth:100,
-        monsterHealth:100
+      playerHealth: 100,
+      monsterHealth: 100
     };
   },
-  methods:{
-      // Function for reducing the monster's health
-      attackMonster(){
-          // Random value that gets taken out of the monster's health
-         const attackValue = Math.floor(Math.random() * (12-5)+5)
+  methods: {
+    // Function for reducing the monster's health
+    attackMonster() {
+      // Const refernece to getRandomValue function, which then gets taken out of the monster's health per attack
+      const attackValue = getRandomValue(5, 12)
+      this.monsterHealth -= attackValue
 
-         this.monsterHealth -= attackValue
-      }
+      // Call attackPlayer function 
+      this.attackPlayer()
+    },
+
+    // Function for reducing the player's health
+    attackPlayer() {
+      // Const refernece to getRandomValue function, which then gets taken out of the player's health per attack
+      const attackValue = getRandomValue(8, 15)
+      this.playerHealth -= attackValue
+    }
   }
 }).mount("#game");
